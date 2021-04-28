e985d1f6e0aa85777d284ed25e0f113b891bca7d
Rodrigo Cananea <rodrigoaguiar35@gmail.com>
2021-04-28 17:34:57 -0300

    Atualizado nome da API

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -21,7 +21,7 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
-window.axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
+window.axios.defaults.baseURL = 'https://shortener.evoluti.info/api/';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
